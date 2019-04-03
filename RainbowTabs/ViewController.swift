7eff23f5fda529e36cdbb6c82f263794280c242e
Soft Dev Student <softdevstudent@robotics-mba-10.local>
2019-04-02 20:27:07 -0400

    Initial Commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Soft Dev Student on 4/2/19.
+//  Copyright © 2019 Soft Dev Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
